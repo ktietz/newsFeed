389d0db07e07a6299679a16fd9b39fcac40b0768
Karl Tietz <ktietz@gmail.com>
2012-11-09 12:21:15 -0500

    Changed a bit of info in menu.py.

--- models/menu.py
+++ models/menu.py
@@ -6,11 +6,11 @@
 #########################################################################
 
 response.title = ' '.join(word.capitalize() for word in request.application.split('_'))
-response.subtitle = T('customize me!')
+response.subtitle = T('Catch up on the latest!')
 
 ## read more at http://dev.w3.org/html5/markup/meta.name.html
-response.meta.author = 'Your Name <you@example.com>'
-response.meta.description = 'a cool new app'
+response.meta.author = 'Karl Tietz <you@example.com>'
+response.meta.description = 'a cool news feed!'
 response.meta.keywords = 'web2py, python, framework'
 response.meta.generator = 'Web2py Web Framework'
 
